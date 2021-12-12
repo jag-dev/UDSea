96b8486a2f2eafaffd9a20837c32c1df4e5e7c46
jag-dev <jag.internet@gmail.com>
2021-12-12 01:16:02 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,123 @@
+import UAuth from '@uauth/js';
+import React, {useEffect, useState} from 'react';
+import Search from './Search'
+
+import './css/Global.css';
+import './css/Nav.css';
+import './css/Login.css';
+
+import logo from './img/uds180.png';
+import background from './img/video.mp4';
+
+  /*/////////////////////////////
+  *   Unstoppable Authentication
+  *//////////////////////////////
+const uauth = new UAuth({
+  clientID: process.env.REACT_APP_CLIENT_ID,
+  clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+  redirectUri: process.env.REACT_APP_REDIRECT_URI,
+  scope: 'openid wallet',
+})
+
+const App: React.FC = () => {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState()
+  const [user, setUser] = useState()
+
+  // Check to see if the user is inside the cache
+  useEffect(() => {
+    setLoading(true)
+    uauth
+      .user()
+      .then(setUser)
+      .catch(() => {})
+      .finally(() => setLoading(false))
+  }, [])
+
+  /*///////////////////////
+  *   Login/out Functions
+  *///////////////////////
+	const handleLogin = () => {
+	setLoading(true)
+	uauth
+	  .loginWithPopup()
+	  .then(() => uauth.user().then(setUser))
+	  .catch(setError)
+	  .finally(() => setLoading(false))
+	}
+
+	const handleLogout = () => {
+		setLoading(true)
+		uauth
+		  .logout()
+		  .then(() => setUser(undefined))
+		  .catch(setError)
+		  .finally(() => setLoading(false))
+	  }
+	  
+  /*///////////////////////
+  *   Display content
+  *///////////////////////
+
+  if (loading) {
+    return <>
+      <video autoPlay muted={true} loop className="video-bg">
+        <source src={background} type="video/mp4" />
+      </video>
+	</>
+  }
+
+  if (error) {
+    console.error(error)
+    return <>{String(error.stack)}</>
+  }
+
+  if (user) {
+    return (
+		<>
+		  <div className="nav"> 
+		  
+		    <ul className="logo-wrapper">
+			  <li className="logo-item"><img className="logo" src={logo} alt="logo"/></li>
+			  <li className="logo-item"><h1 className="logo-title">UDSea</h1></li>
+			  <li className="logo-item"><span className="logo-title subtitle">| NFT Gallery</span></li>
+			  <hr/>
+			</ul>
+			
+		    <ul className="auth-wrapper">
+			  <li className="auth-item domain">{user.sub}</li>
+			  <li className="auth-item"><button className="logout-btn" onClick={handleLogout}>Logout</button></li>
+			</ul>
+			
+		  </div>
+		  
+		  <Search />
+		</>
+    )
+  }
+
+  return (
+    <>
+	{/* Video by Taryn Elliott from Pexels */}
+	  <video autoPlay muted={true} loop className="video-bg">
+        <source src={background} type="video/mp4" />
+      </video>
+	  <div className="login-wrapper positioned">
+	    <div className="login">
+		  <div className="login-image">
+		    <img src={logo} alt="logo" />
+		  </div>
+		  <div className="login-title">
+		    <h1>UDSea</h1>
+			<h2>NFT Gallery</h2>
+			<h3>View other Unstoppable Domains family NFT collections by simply searching for their crypto address</h3>
+			<h4>Sign-In Options</h4>
+		  </div>
+          <button className="login-btn positioned" onClick={handleLogin}></button>
+		</div>
+	  </div>
+	</>
+  )
+}
+
+export default App;
---
+++ src/Search.jsx
@@ -0,0 +1,159 @@
+import React from 'react';
+import Resolution from '@unstoppabledomains/resolution';
+import { NftGallery } from 'react-nft-gallery';
+
+import './css/Search.css';
+import logo from './img/uds180.png';
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+	
+    this.state = { domain: "",
+	               address: "",
+				   error: false,
+                   searched: false };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+	this.handleCleanup = this.handleCleanup.bind(this);
+	this.displayLookup = this.displayLookup.bind(this)
+	this.displayGallery = this.displayGallery.bind(this);
+	this.displayError = this.displayError.bind(this);
+	this.resolve = this.resolve.bind(this);
+  }
+
+  /*///////////////////////
+   *   Search functions 
+   *///////////////////////
+  handleChange(event) {
+    this.setState({domain: event.target.value});
+	this.setState({address: ''});
+	this.domain = event.target.value;
+  }
+  
+  handleCleanup = () => {
+    const elements = document.getElementsByClassName('rnftg-item');
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+  }
+  
+  handleSubmit(event) {
+	this.handleCleanup();
+	this.setState({searched: true});
+	this.resolve(this.domain);
+    event.preventDefault();
+  }
+  
+  /*///////////////////////
+   *   Display functions 
+   *///////////////////////
+  displayGallery = () => {
+	if (this.state.searched) { 
+	  return ( <div id="gallery"><NftGallery ownerAddress={this.state.address} /></div> );
+	}
+  }
+  
+  displayError = () => {
+	if (this.state.error) {
+	  return (
+	    <>
+	      <div className="error-wrapper">
+		    <h1>Uh-oh!</h1>
+			<h2>That domain is not registered with Unstoppable</h2>
+		  </div>
+		</>
+	  );
+	}
+  }
+  
+  displayLookup = () => {
+	const etherlink = 'https://etherscan.io/address/' + this.state.address;
+	
+    if (this.state.searched && !this.state.error) {
+	  return (
+	    <>
+		  <ul className="lookup-wrapper">
+		    <li>
+			  <span className="lookup-title">Domain </span>
+			  <span className="lookup-content">{this.domain}</span>
+			</li>
+			<li>
+			  <span className="lookup-title">Address</span>
+			  <span className="lookup-content">{this.state.address}</span>
+			</li>
+			<li className="lookup-es">
+			  <a href={etherlink} target="_blank" className="lookup-esa">View on Etherscan</a>
+			</li>
+		  </ul>
+		  <hr style={{margin: "0 1.5em 0 1.5em"}}/>
+		</>
+	  );
+	}
+  }
+  
+  displayDesc = () => {
+    if (!this.state.searched || this.state.error) {
+	  return (
+	    <>
+		  <p className="description">
+		  This application was built for the "Login with Unstoppable" Gitcoin hackathon
+		  </p>
+		</>
+	  );
+	}
+  }
+  
+  /*///////////////////////
+   *   Resolve function
+   *///////////////////////
+  resolve = (domain) => {
+    let resolution = new Resolution({ blockchain: {
+      uns: {
+        url: "https://mainnet.infura.io/v3/12351245223",
+        network: "mainnet"
+      }
+    }});
+	
+	resolution.isRegistered(domain).then((result) => {
+		if (result) {
+			this.setState({error: false});
+			resolution.addr(domain, "eth")
+	                  .then(addr => this.setState({address: addr}));
+		} else {
+			// popup error?
+			this.setState({error: true});
+		}
+	});
+    
+  }
+  
+  /*///////////////////////
+  *   Search return
+  *///////////////////////
+	render() { 
+		return (
+		  <form className="search-form" onSubmit={this.handleSubmit}>
+		    <div className="search-wrapper">
+			  <ul className="search-header">
+			    <li><img className="" src={logo} alt="logo"/></li>
+			    <li><h1>UDSea</h1></li>
+				<hr style={{margin: "1.5em 15em 0 15em"}}/>
+				<li className="search-desc"><h2>Sail the blockchain for other users NFT collections by searching their Unstoppable Domains address</h2></li>
+
+		      </ul>
+			  <input type="text" className="search-bar" placeholder="Domain address..." value={this.state.domain} onChange={this.handleChange} />
+			  <input type="submit" className="search-btn" value="Search" />
+			</div>
+			
+			{ this.displayError() }
+			{ this.displayLookup() }
+			{ this.displayGallery() }
+			{ this.displayDesc() }
+		  </form>
+		);
+	}
+	
+}
+export default Search;
